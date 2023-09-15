d3bca12b65884334ca7830dc712f0df469bf634e
Zarak Ansari <zarak.ansari@yahoo.com>
2023-09-15 11:14:53 +0500

    completed 9-5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,26 +1,4 @@
-const calculateBmi = (heightInCentimeters: number, weightInKg: number): string => {
-    const bmi = weightInKg / (heightInCentimeters / 100) ** 2
-
-    if(bmi < 16){
-        return "Underweight (Severe thinness)"
-    } else if (bmi < 17) {
-        return "Underweight (Moderate thinness)"
-    } else if (bmi < 18.5) {
-        return "Underweight (Mild thinness)"
-    } else if (bmi < 25) {
-        return "Normal range (healthy weight)"
-    } else if (bmi < 30) {
-        return "Overweight (Pre-obese)"
-    } else if (bmi < 35) {
-        return "Obese (Class I)"
-    } else if (bmi < 40) {
-        return "Obese (Class II)"
-    } else if (bmi >= 40) {
-        return "Obese (Class III)"
-    } else {
-        return `Something went wrong: inputs: ${heightInCentimeters} - ${weightInKg}`
-    }
-}
+import { calculateBmi } from "./calculateBmi"
 
 const processArguments = (args: string[]) => {
 
@@ -38,7 +16,8 @@ const processArguments = (args: string[]) => {
 
 }
 
-const {height, weight} = processArguments(process.argv)
-
-console.log(calculateBmi(height, weight))
-
+if(process.argv){
+    const {height, weight} = processArguments(process.argv)
+    
+    console.log(calculateBmi(height, weight))
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { calculateBmi } from "./calculateBmi";
 
 const app = express()
 
@@ -7,7 +7,25 @@ app.get('/hello', (_, res) => {
     res.send("Hello Full Stack!"); 
 })
 
-const PORT = 3003
+app.get('/bmi', (req, res) => {
+    const height = Number(req.query.height)
+    const weight = Number(req.query.weight)
+
+    if(isNaN(height) || isNaN(weight)){
+        res.status(400).json({
+            error:"malformatted parameters"
+        })
+    } else {
+        res.json({
+            height,
+            weight,
+            bmi: calculateBmi(height, weight)
+        })
+    }
+
+})
+
+const PORT = 3002
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)

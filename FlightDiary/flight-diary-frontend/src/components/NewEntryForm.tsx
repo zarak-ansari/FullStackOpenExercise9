127c457afe8ece09c189e56fd0e1fd93416943de
Zarak Ansari <zarak.ansari@yahoo.com>
2023-11-01 12:28:39 +0500

    completed till 9-19

--- FlightDiary/flight-diary-frontend/src/components/NewEntryForm.tsx
+++ FlightDiary/flight-diary-frontend/src/components/NewEntryForm.tsx
@@ -46,9 +46,39 @@ const NewEntryForm = (props: NewEntryFormProps) => {
             <h2>Add New Diary Entry</h2>
             {errorMessage && <p style={{color:"red"}}>{errorMessage}</p>}
             <form onSubmit={addNewEntry}>
-                <p>Date: <input value={date} onChange={e => setDate(e.target.value)} /></p>
-                <p>Weather: <input value={weather} onChange={e => setWeather(e.target.value)} /></p>
-                <p>Visibility: <input value={visibility} onChange={e => setVisibility(e.target.value)} /></p>
+                <p>Date: <input type="date" value={date} onChange={e => setDate(e.target.value)} /></p>
+                <p>Weather:
+                    {Object.values(Weather).map(weatherValue => {
+                        return(
+                        <span key={weatherValue}>
+                            <input 
+                                type="radio"
+                                name="weather"
+                                value={weatherValue}
+                                id={weatherValue}
+                                checked={weather === weatherValue}
+                                onChange={e => setWeather(e.target.value)}
+                            />
+                            {weatherValue}
+                        </span>)
+                    })}
+                </p>
+                <p>Visibility: 
+                    {Object.values(Visibility).map(visibilityValue => {
+                        return(
+                        <span key={visibilityValue}>
+                            <input 
+                                type="radio"
+                                name="visibility"
+                                value={visibilityValue}
+                                id={visibilityValue}
+                                checked={visibility === visibilityValue}
+                                onChange={e => setVisibility(e.target.value)}
+                            />
+                            {visibilityValue}
+                        </span>)
+                    })}
+                </p>
                 <p>Comment: <input value={comment} onChange={e => setComment(e.target.value)} /></p>
                 
                 <button type="submit">Add New Diary Entry</button>
